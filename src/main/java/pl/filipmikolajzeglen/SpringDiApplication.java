2089c1824e67715f742113c9700e82281cc1e72c
Filip Żegleń <filipmikolajgabriel@gmail.com>
2021-02-28 12:30:20 +0100

    Initial commit

---
+++ src/main/java/pl/filipmikolajzeglen/SpringDiApplication.java
@@ -0,0 +1,21 @@
+package pl.filipmikolajzeglen;
+
+import java.util.List;
+
+import org.springframework.context.annotation.*;
+import pl.filipmikolajzeglen.beans.NamesRepository;
+
+@Configuration
+@ComponentScan
+public class SpringDiApplication {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+                SpringDiApplication.class);
+
+        NamesRepository namesRepo = ctx.getBean(NamesRepository.class);
+        List<String> allNames = namesRepo.getAll();
+        allNames.forEach(System.out::println);
+
+        ctx.close();
+    }
+}
